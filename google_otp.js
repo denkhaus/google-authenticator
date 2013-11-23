7b44f45c82b8090017e6535d336c3300c59f02ff
denkhaus <peristaltic@gmx.net>
2013-11-23 19:51:31 +0100

    changed description

--- google_otp.js
+++ google_otp.js
@@ -2,10 +2,10 @@ var notp = Npm.require('notp');
 
 GoogleOTPAPI = function() {
     this.api = notp.totp;
-}
+};
 
 GoogleOTPAPI.prototype.verify = function(token, key) {
 
     var res = this.api.verify(token, key);
     return res;
-}
+};
